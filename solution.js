6b3b8db6a252a2e6f84f030990ae8ae825792308
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-08 17:10:53 -0800

    abbreviate-solution

--- solution.js
+++ solution.js
@@ -1,18 +1,8 @@
-function solve(arr){
-  for(var i = 0; i < arr.length; i++) {
-    x = arr[i];
-
-    for(var j = 0; j < arr.length; j++) {
-      y = arr[j];
-
-      if (x === -y) {
-        var a = arr.indexOf(x);
-        arr.splice(a,1);
-
-        var b = arr.indexOf(y);
-        arr.splice(b,1);
-      }
-    }
-  }
-  return arr[arr.length - 1];
-};
+function abbrevName(name){
+  var str = name;
+  var strSplit = str.split(' ');
+  var first = strSplit[0];
+  var last = strSplit[1];
+  
+  return first.charAt(0).toUpperCase() + '.' + last.charAt(0).toUpperCase();
+}
