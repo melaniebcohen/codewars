33d6c3269ef73d912be2d1b33cee779de70e571e
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-08 17:15:27 -0800

    replace dots solution

--- solution.js
+++ solution.js
@@ -1,8 +1,3 @@
-function abbrevName(name){
-  var str = name;
-  var strSplit = str.split(' ');
-  var first = strSplit[0];
-  var last = strSplit[1];
-  
-  return first.charAt(0).toUpperCase() + '.' + last.charAt(0).toUpperCase();
+var replaceDots = function(str) {
+  return str.replace(/\./g, '-');
 }
