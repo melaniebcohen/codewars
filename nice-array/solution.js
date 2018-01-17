2ef5983d2940f6e403c0eb2e12bd7994e122faf5
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-17 12:35:31 -0800

    no solution yet

--- nice-array/solution.js
+++ nice-array/solution.js
@@ -1,3 +1,24 @@
-var replaceDots = function(str) {
-  return str.replace(/\./g, '-');
+function isNice(arr){
+  var niceArr = [];
+  var counter = 0;
+
+  for(var i = 0; i < arr.length; i++) {
+    console.log('i',i)
+    var x = arr[i] - 1;
+    var y = arr[i] + 1;
+
+    for(var j = 0; j < arr.length; j++) {
+      if (x === arr[j] || y === arr[j]) {
+        //
+      }
+    }
+  }
+
+  // if (arr.length === niceArr.length) {
+  //   return true;
+  // } else if (arr.length === 0) {
+  //   return false;
+  // } else {
+  //   return false;
+  // }
 }
