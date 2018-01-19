152110cbac9295ea06104bf2c34b3dd2b5e69721
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-18 19:10:07 -0800

    updates

--- format-words/solution.js
+++ format-words/solution.js
@@ -1,3 +1,19 @@
 function formatWords(words){
-  
+
+  if (words.length === 0) {
+    return '""';
+  } else if (words.length === 1) {
+    return words.join('');
+  } else if (words.length === 2) {
+    return words.join(' and ');
+  } else {   // start here
+    let newArr = ' and ' + words.pop();
+    console.log(words, newArr);
+
+    words.join('');
+    return words.join(', ') + newArr;
+    // console.log(newArr)
+  }
 }
+formatWords(['ninja', 'samurai', 'ronin'])
+formatWords(['ninja', '', 'ronin'])
