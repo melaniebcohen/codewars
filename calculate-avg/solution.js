7d05ad54dcf9ba3fbaad5ef245af85d20a86c6ca
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-21 16:21:14 -0800

    solutions

--- calculate-avg/solution.js
+++ calculate-avg/solution.js
@@ -1,10 +1,19 @@
-// Can't figure out why 0 is not appearing in new arrays.
+function find_average(array) {
+  var sum = array.reduce(function(prev, cur) {
+    return prev + cur;
+  })
+  return sum / array.length;
+}
 
-function filter_list(l) {
-  var mapped = l.filter(function(item) {
-    if (typeof(item) === 'number') {
-      return item;
-    }
-  });
-  return mapped;
+// refactored for ES6
+function find_average(array) {
+  var sum = array.reduce((prev, cur) => {
+    return prev + cur;
+  })
+  return sum / array.length;
+}
+
+// refactored again
+function find_average(array) {
+  return (array.reduce((prev, cur) => { return prev + cur; })) / array.length;
 }
--- list-filtering/solution.js
+++ list-filtering/solution.js
@@ -1,8 +1,10 @@
+// Can't figure out why 0 is not appearing in new arrays.
+
 function filter_list(l) {
-  // Return a new array with the strings filtered out
-  return l.filter(function(n) {
-    if (Number.isInteger(n) === true) {
-      return n;
+  var mapped = l.filter(function(item) {
+    if (typeof(item) === 'number') {
+      return item;
     }
-  })
+  });
+  return mapped;
 }
